80c29988342f6d5155e920cbf10174a936e2bd25
Huizhe Xiao <huizhe.xiao@gmail.com>
2018-05-26 05:33:54 +0800

    sample for truffle-secret.json

--- truffle.js
+++ truffle.js
@@ -1,14 +1,21 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var secret = require('./secret.json');
+var secret = require('./truffle-secret.json');
 
 module.exports = {
   networks: {
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(secret.truffle.ropsten.mnemonic, "https://ropsten.infura.io/" + secret.truffle.ropsten.infura_api_key)
+        return new HDWalletProvider(secret.ropsten.mnemonic, "https://ropsten.infura.io/" + secret.ropsten.infura_api_key)
       },
       network_id: 3,
-      from: secret.truffle.ropsten.sender_address
+      from: secret.ropsten.sender_address
+    },
+    live: {
+      provider: function() {
+        return new HDWalletProvider(secret.live.mnemonic, "https://mainnet.infura.io/" + secret.live.infura_api_key)
+      },
+      network_id: 1,
+      from: secret.live.sender_address
     }
   }
 };
